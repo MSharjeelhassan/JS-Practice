8f5552898f669b3017238427cbc39a40759cebec
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2023-12-12 20:23:44 +0500

    ch-5 completed

---
+++ ch-4/app.js
@@ -0,0 +1,33 @@
+// 1. Correct this statement.
+// var product cost = 3.45;
+
+var productCost = 3.45;
+
+// 2. Rewrite this using camelCase.
+// var Nameofband;
+
+var nameOfBand;
+
+var number10 = 10;
+
+var sharjeelHassan;
+
+// Variable Legal and Illegal types
+
+// Legal
+
+var han;
+var han10;
+var han$10;
+var han_10;
+var han_;
+var han_han;
+
+
+// Illegal
+// var 10;
+// var 10a;
+// var var;
+// var if;
+
+
