54b12b1a4398315682169ef80a383d07781fb7a5
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2023-12-13 19:26:17 +0500

    Ch-5 finished

---
+++ ch-5/app.js
@@ -0,0 +1,23 @@
+// What is the name and symbol of the arithmetic operator that gives you the remainder when one number is divided by another ?
+// Modolus
+
+// What is the value of num?
+var num = 20 % 6;
+console.log(num);
+
+// In a single statement, declare the variable largeNum and assign it the result of 1,000 multiplied by 2,000.
+var largeNum = 1000*2000;
+console.log(largeNum);
+
+// Assign to a variable the value represented by one variable subtracted from the value represented by another variable
+var num2 = largeNum - num;
+console.log(num2);
+
+// Assign to a variable the remainder when one number is divided by another. The variable hasn't been declared beforehand. Make up the variable name.
+var mod = 10 % 3;
+console.log(mod);
+
+// Code an alert that displays the result of a multiplication on 2 numbers.
+var multiple = 4 * 5;
+alert(multiple + " Multiplication of two numbers");
+
