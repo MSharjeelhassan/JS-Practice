d7d42bb8e190d464d2d96315214abe0a66e5a9c0
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2023-12-13 21:21:06 +0500

    Ch-9 question 3 completed

---
+++ ch-8/app.js
@@ -0,0 +1,21 @@
+var num = "2" + "2";
+console.log(num);
+
+var message = "Hello," + "Dolly";
+console.log(message);
+alert(message);
+
+alert("33" + 3);
+
+var p = "Pakistan";
+var z = "Zindabad";
+
+alert(p + " " + z);
+
+var her0 = "Audi" + 3;
+console.log(her0);
+
+var he = "Hello ";
+var wo = " World";
+var heWo = he+wo;
+console.log(heWo);
---
+++ ch-9/app.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter first name");
+var country = prompt("Country", "China");
+var yourName = prompt("Enter Your Name");
+var pro = prompt("I didnt understand this one");
+
