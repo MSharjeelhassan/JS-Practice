8c1ff457ea286e555fa11992d0e40d69cc55307d
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2023-12-13 20:09:50 +0500

    ch-7 finished

---
+++ ch-7/app.js
@@ -0,0 +1,20 @@
+var calculatedNum = 2 + (2 * 6);
+console.log(calculatedNum);
+
+var calculatedNum = (2 + 2) * 6;
+console.log(calculatedNum);
+
+var calculatedNum = (2 + 2) * (4 + 2);
+console.log(calculatedNum);
+
+var calculatedNum = ((2 + 2) * 4) + 2;
+console.log(calculatedNum);
+
+var cost = ((2 + 2) *( 4 + 10));
+console.log(cost);
+
+var cost =  2 + (2 * 4) + 10;
+console.log(cost);
+
+var cost = 4 /( 2 * 4);
+console.log(cost);
